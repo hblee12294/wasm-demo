9fbdb89e0532759bcd36c2990f612294f006a321
Hblee <hblee12294@gmail.com>
2023-06-26 13:11:51 -0700

    feat: test remove main and include from another file

--- main.c
+++ main.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
+#include <emscripten/emscripten.h>
+#include "util.c"
 
-int main(int argc, char ** argv) {
-  printf("Hello World\n");
+// int main(int argc, char **argv)
+// {
+//   printf("Hello World\n");
+// }
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+  void EMSCRIPTEN_KEEPALIVE myFunction(int argc, char **argv)
+  {
+    printSomething();
+  }
+
+  char *EMSCRIPTEN_KEEPALIVE getString()
+  {
+    return "Hooooooooo Ya!!!!";
+  }
+
+#ifdef __cplusplus
 }
+#endif
---
+++ util.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void printSomething()
+{
+  printf("Called from another file\n");
+}
