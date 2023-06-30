03424a4c23c18a39fb130c6c088963bf108acb10
Hblee <hblee12294@gmail.com>
2023-06-29 17:46:52 -0700

    feat: bind class

--- main_bind.cpp
+++ main_bind.cpp
@@ -10,7 +10,7 @@ using namespace emscripten;
 //   printf("Hello World\n");
 // }
 
-void myFunction()
+void printString()
 {
   printSomething();
 }
@@ -25,9 +25,39 @@ std::string getString()
   return "Hooooooooo Ya!!!!";
 }
 
+class C {
+public:
+  void print()
+  {
+    printf("From class C!!!\n");
+  };
+};
+
+auto c = new C();
+
+C *getPointer()
+{
+  return c;
+}
+
+void printPointer(C *ptr)
+{
+  printf("Pointer: %p\n", ptr);
+}
+
+void printFromClass(C *ptr)
+{
+  ptr->print();
+}
+
 EMSCRIPTEN_BINDINGS(my_module) {
-  function("myFunction", &myFunction);
+  class_<C>("C");
+
+  function("printString", &printString);
   function("add", &add);
-  function("getString", &getString, allow_raw_pointers());
+  function("getString", &getString);
+  function("getPointer", &getPointer, allow_raw_pointers());
+  function("printPointer", &printPointer, allow_raw_pointers());
+  function("printFromClass", &printFromClass, allow_raw_pointers());
 }
 
